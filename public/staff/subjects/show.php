e1ee642da602a91c2745a1b61c93966a6fbe8e5b
SBesedin <sbesedin@gorvodokanal.com>
2020-03-11 13:42:59 +0700

    getting single rows from subjects

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -2,16 +2,36 @@
 
 $id = $_GET['id'] ?? '1';
 
+$subject = find_subject_by_id($id);
+
 $page_title = 'Show Subject';
 
 include(SHARED_PATH . '/staff_header.php');?>
 
     <div id="content">
         <a href="<?php echo WWW_ROOT . '/staff/subjects/index.php' ?>" class="back-link">&laquo; Back to list</a>
+        <div class="subject show">
+
+            <h1>Subject: <?php echo h($subject['menu_name']); ?></h1>
+
+            <div class="attributes">
+                <dl>
+                    <dt>Menu Name</dt>
+                    <dd><?php echo h($subject['menu_name']) ?></dd>
+                </dl>
+                <dl>
+                    <dt>Position</dt>
+                    <dd><?php echo h($subject['position']) ?></dd>
+                </dl>
+                <dl>
+                    <dt>Visible</dt>
+                    <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
+                </dl>
+            </div>
+
+        </div>
     </div>
 
-    <div class="subject show">
-        Subject ID: <?php echo h($id); ?>
-    </div>
+    
 
 <?php include(SHARED_PATH . '/staff_footer.php');
